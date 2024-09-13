efb05cbc8011407749866cff63e2d56f90deed35
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-13 14:03:53 +0300

    add solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogramClass:
+    @pytest.mark.parametrize(
+        "word, result",
+        [
+            ("playgrounds", True),
+            ("pLaYgRoUnDs", True),
+            ("look", False),
+            ("Adam", False),
+            ("", True)
+        ]
+    )
+    def test_is_isogram(self, word: str, result: bool) -> None:
+        assert (
+            is_isogram(word) == result
+        ), f"{is_isogram(word)} should return {result}"
+
+
+class TestIsIsogramError:
+    def test_if_isogram_take_int_param(self) -> None:
+        with pytest.raises(AttributeError):
+            is_isogram(12)
